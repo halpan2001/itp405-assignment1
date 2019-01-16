5833579ae7564ee1d3f75927fdd870c571a87b82
halpan2001 <halpan@usc.edu>
2019-01-15 20:53:53 -0800

    updated title

--- tracks.php
+++ tracks.php
@@ -36,7 +36,7 @@
 <html lang="en" dir="ltr">
 <head>
   <meta charset="utf-8">
-  <title>Week 2</title>
+  <title>Assignment 1: PDO</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
 </head>
 <body>
